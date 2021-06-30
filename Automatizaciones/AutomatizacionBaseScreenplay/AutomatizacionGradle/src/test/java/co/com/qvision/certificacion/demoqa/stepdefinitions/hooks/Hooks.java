c9aa0367975e0f8e47f11dc29f7a5c555d5b7265
Karen <karengom7@gmail.com.co>
2021-06-30 14:07:11 -0500

    Automatización

--- Automatizaciones/AutomatizacionBaseScreenplay/AutomatizacionGradle/src/test/java/co/com/qvision/certificacion/demoqa/stepdefinitions/hooks/Hooks.java
+++ Automatizaciones/AutomatizacionBaseScreenplay/AutomatizacionGradle/src/test/java/co/com/qvision/certificacion/demoqa/stepdefinitions/hooks/Hooks.java
@@ -10,25 +10,32 @@ import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 public class Hooks {
 
     //TextBox
-    @Before(value = "@textBox", order = 0)
-    public void abrirNavegador() {
-        OnStage.setTheStage(new OnlineCast());
-        OnStage.theActorCalled("Mateo");
-        theActorInTheSpotlight().attemptsTo(Open.url("https://demoqa.com/"));
-    }
+//    @Before(value = "@textBox", order = 0)
+//    public void abrirNavegador() {
+//        OnStage.setTheStage(new OnlineCast());
+//        OnStage.theActorCalled("Mateo");
+//        theActorInTheSpotlight().attemptsTo(Open.url("https://demoqa.com/"));
+//    }
 //
-//    //Practice Form
+    //Practice Form
 //    @Before(value = "@practiceForm", order = 0)
 //    public void abrirNavegador() {
 //        OnStage.setTheStage(new OnlineCast());
 //        OnStage.theActorCalled("Armando");
 //        theActorInTheSpotlight().attemptsTo(Open.url("https://demoqa.com/"));
 //    }
-
-//    @Before(value = "@test1", order = 1)
-//    public void abrirNavegadorTextBox() {
+    //Radio Buttton
+//    @Before(value = "@radioButton", order = 1)
+//    public void abrirNavegador() {
 //        OnStage.setTheStage(new OnlineCast());
 //        OnStage.theActorCalled("Armando");
 //        theActorInTheSpotlight().attemptsTo(Open.url("https://demoqa.com/"));
 //    }
+
+    @Before(value = "@uploadDown", order = 1)
+    public void abrirNavegador() {
+        OnStage.setTheStage(new OnlineCast());
+        OnStage.theActorCalled("Armando");
+        theActorInTheSpotlight().attemptsTo(Open.url("https://demoqa.com/"));
+    }
 }
