e4ea378442ee06e335f83e7575082558127be8c5
Karen <karengom7@gmail.com.co>
2021-07-15 10:11:08 -0500

    Proyecto Gradle

---
+++ Automatizaciones/AutomatizacionBasePOM/AutomatizacionGradle/src/main/java/co/com/qvision/certificacion/demoqa/pages/userinterfaces/forms/DemoQAFormsUI.java
@@ -0,0 +1,34 @@
+package co.com.qvision.certificacion.demoqa.pages.userinterfaces.forms;
+
+import net.serenitybdd.core.pages.WebElementFacade;
+import net.thucydides.core.pages.PageObject;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class DemoQAFormsUI extends PageObject {
+
+    @FindBy(xpath = "//*[@id='firstName']")
+    public WebElementFacade nombre;
+
+    @FindBy(xpath = "//*[@id='lastName']")
+    public WebElementFacade apellido;
+
+    @FindBy(xpath = "//*[@id='userEmail']")
+    public WebElementFacade email;
+
+    @FindBy(xpath = "//*[@id='userNumber']")
+    public WebElementFacade telefono;
+
+    public WebElement getGenero(String dato) {
+        return getDriver().findElement(By.xpath("//*[@for='gender-radio-"+dato+"']"));
+    }
+
+    public WebElement getCampoID(String dato) {
+        return getDriver().findElement(By.xpath("//*[@id='"+dato+"']"));
+    }
+
+    public WebElement getCampoTabla(String dato1 ,String dato2) {
+        return getDriver().findElement(By.xpath(" //table/tbody/tr["+dato1+"]/td["+dato2+"]"));
+    }
+}
